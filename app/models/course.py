ef4d73fc33e0082fa1aa12b68b4c4b1fc742bc51
xMintTea <minttea77105110116@gmail.com>
2026-02-27 22:10:08 +0800

    feat: added course models

---
+++ app/models/course.py
@@ -0,0 +1,101 @@
+from sqlalchemy import String,Text, ForeignKey, DateTime, text
+from sqlalchemy.orm import Mapped, mapped_column, relationship, declared_attr
+from typing import Optional, List
+from datetime import datetime
+
+from database import BaseModel
+from models.user import User
+
+
+
+class SectionContent(BaseModel):
+    __abstract__ = True
+    
+    section_id: Mapped[int] = mapped_column(ForeignKey("course_sections.id"))
+    creation_date: Mapped[datetime] = mapped_column(
+        DateTime,
+        server_default=text("CURRENT_TIMESTAMP")
+    )
+    last_change_date: Mapped[Optional[datetime]]
+    order: Mapped[int]
+    visibility_id: Mapped[int] = mapped_column(ForeignKey("visibility.id"))
+ 
+
+
+   
+class Course(BaseModel):
+    __tablename__ = "courses"
+    
+    name: Mapped[str] = mapped_column(String(256))
+    description: Mapped[Optional[str]] = mapped_column(Text)
+    secret: Mapped[Optional[str]]
+    visibility_id: Mapped[int] = mapped_column(ForeignKey("visibility.id"))
+    creation_date: Mapped[datetime] = mapped_column(
+        DateTime,
+        server_default=text("CURRENT_TIMESTAMP")
+    )
+    
+    visibility: Mapped["Visibility"] = relationship()
+    
+
+
+
+class CourseSection(BaseModel):
+    __tablename__ = "course_sections"
+    
+    course_id: Mapped[int] = mapped_column(ForeignKey("courses.id"))
+    
+    pages: Mapped["SectionPage"] = relationship()
+    tests: Mapped["Test"] = relationship()
+
+
+
+class SectionPage(SectionContent):
+    __tablename__ = "section_pages"
+    
+    
+    # TODO: Good for now. Gonna figure out later how to store page content there. 
+
+
+class Test(SectionContent):
+    __tablename__ = "tests"
+    
+    deadline_date: Mapped[Optional[datetime]]
+
+
+class TestResult(BaseModel):
+    __tablename__ = "test_results"
+    
+    test_id: Mapped[int] = mapped_column(ForeignKey("tests.id"))
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    start_time: Mapped[datetime]
+    end_time: Mapped[datetime]
+    answers: Mapped[str] = mapped_column(Text)
+
+
+class Visibility(BaseModel):
+    __tablename__ = "visibility"
+    
+    name: Mapped[str]
+
+
+class CourseUser(BaseModel):
+    __tablename__ = "course_users"
+    
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    course_id: Mapped[int] = mapped_column(ForeignKey("courses.id"))
+    access_level_id: Mapped[int] = mapped_column(ForeignKey("access_levels.id"))
+    access_status_id: Mapped[int] = mapped_column(ForeignKey("access_statuses.id"))
+    date_of_join: Mapped[datetime]
+
+
+class AccessLevel(BaseModel):
+    __tablename__ = "access_levels"
+    
+    name: Mapped[str]
+
+
+class AccessStatus(BaseModel):
+    __tablename__ = "access_statuses"
+    
+    name: Mapped[str]

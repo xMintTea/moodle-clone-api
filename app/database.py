1950cef286dc01ce061d41a5ce469dbd19522f1d
xMintTea <minttea77105110116@gmail.com>
2026-02-27 22:09:37 +0800

    feat: added database file

---
+++ app/database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase, Mapped, mapped_column
+
+from core.config import config
+
+
+SQL_DB_URL = config.db_url
+
+engine = create_engine(SQL_DB_URL, connect_args={
+    "check_same_thread" : False
+}, echo=True)
+
+
+class Base(DeclarativeBase):
+    ...
+    
+
+class BaseModel(Base):
+    __abstract__ = True
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+session_local = sessionmaker(autoflush=False, autocommit=False, bind=engine)

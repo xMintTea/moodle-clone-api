094598d761e66e45f6b46fd370c5f4ce577ace33
xMintTea <minttea77105110116@gmail.com>
2026-03-02 20:49:16 +0800

    feat: added user schemas

---
+++ app/schemas/user.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, Field, EmailStr
+from typing import Annotated
+
+from models.context.enums import UserStatus, UserType
+
+class UserSchema(BaseModel):
+    first_name: Annotated[str,Field(..., title="Имя пользователя",
+                                    min_length=2,
+                                    max_length=50)]
+    middle_name: Annotated[str,Field(..., title="Отчество пользователя",
+                                    min_length=2,
+                                    max_length=50)]
+    last_name: Annotated[str,Field(..., title="Фамилия пользователя",
+                                    min_length=2,
+                                    max_length=50)]
+    email: Annotated[EmailStr, Field(..., max_length=32)]
+    password: Annotated[str, Field(..., min_length=8, max_length=18)]
+    user_type: Annotated[UserType, Field(...)]
+    user_status: Annotated[UserStatus, Field(...)]

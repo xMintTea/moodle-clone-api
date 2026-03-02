6b0828c38971264c7a0325cdbbf9907fd4b7ea7f
xMintTea <minttea77105110116@gmail.com>
2026-03-02 20:48:55 +0800

    feat: added password utils

---
+++ app/utils/password.py
@@ -0,0 +1,15 @@
+import bcrypt
+
+def hash_password(
+    password: str
+) -> bytes:
+    salt = bcrypt.gensalt()
+    pwd_bytes: bytes = password.encode()
+    return bcrypt.hashpw(pwd_bytes, salt)
+
+
+def validate_password(
+    password: str,
+    hashed_password: bytes
+) -> bool:
+    return bcrypt.checkpw(password.encode(), hashed_password)

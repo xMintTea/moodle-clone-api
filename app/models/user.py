1fe681dbedce3df2d2d91ce5c8a4f0282efa5cc4
xMintTea <minttea77105110116@gmail.com>
2026-03-02 16:01:29 +0800

    feat: replaced tables with enums and removed old tables

--- app/models/user.py
+++ app/models/user.py
@@ -1,8 +1,9 @@
-from sqlalchemy import String, ForeignKey
+from sqlalchemy import String, Enum
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from pydantic import EmailStr
 
 from database import BaseModel
+from models.context.enums import UserStatus, UserType
 
 
 
@@ -14,28 +15,12 @@ class User(BaseModel):
     last_name: Mapped[str] = mapped_column(String(50))
     email: Mapped[EmailStr] = mapped_column(String())
     password: Mapped[str]
-    salt: Mapped[str]
-    user_type_id: Mapped[int] = mapped_column(ForeignKey("user_types.id"))
-    user_status_id: Mapped[int] = mapped_column(ForeignKey("user_statuses.id"))
-    
-    
-    user_type: Mapped["UserType"] = relationship()
-    user_status: Mapped["UserStatus"] = relationship()
+    salt: Mapped[str]   
+    user_type: Mapped[UserType] = mapped_column(Enum(UserType))
+    user_status: Mapped[UserStatus] = mapped_column(Enum(UserStatus))
     
 
     @property
     def full_name(self) -> str:
         return f"{self.first_name} {self.last_name} {self.middle_name}"
     
-
-
-class UserType(BaseModel):
-    __tablename__ = "user_types"
-    
-    name: Mapped[str]
-
-
-class UserStatus(BaseModel):
-    __tablename__ = "user_statuses"
-    
-    name: Mapped[str]

248b0e186141b7d2fd48026eff22ccf9b84f7d2f
xMintTea <minttea77105110116@gmail.com>
2026-03-02 20:49:29 +0800

    feat: added user service

---
+++ app/service/user_service.py
@@ -0,0 +1,66 @@
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import NoResultFound
+
+from models.user import User
+from schemas.user import UserSchema
+from utils import password as pw_utils
+
+
+class UserService:
+    def __init__(self, session: Session) -> None:
+        self._db = session
+    
+    
+    def list_users(self) -> list[User]:
+        return self._db.query(User).all()
+    
+    def get_user(self, user_id: int) -> User | None:
+        return self._db.query(User).filter(User.id == user_id).first()
+    
+    def create_user(self, user_schema: UserSchema) -> User:
+        
+        user = User(
+            first_name=user_schema.first_name,
+            middle_name=user_schema.middle_name,
+            last_name=user_schema.last_name,
+            email=user_schema.email,
+            password=pw_utils.hash_password(user_schema.password),
+            user_status=user_schema.user_status,
+            user_type=user_schema.user_type
+        )
+        
+        self._db.add(user)
+        self._db.commit()
+        self._db.refresh(user)
+        return user
+
+    def update_user(self, user_id: int, user_schema: UserSchema) -> User:
+        user = self._get_user_or_raise(user_id)
+        
+        user.first_name = user_schema.first_name
+        user.middle_name = user_schema.middle_name
+        user.last_name = user_schema.last_name
+        user.email = user_schema.email
+        user.user_status = user_schema.user_status
+        user.user_type = user_schema.user_type
+            
+        if not pw_utils.validate_password(user_schema.password, user.password):
+            user.password = pw_utils.hash_password(user_schema.password)
+            
+        self._db.commit()
+        self._db.refresh(user)
+        return user
+    
+
+    
+    def delete_user(self, user_id: int):
+        user = self._get_user_or_raise(user_id)
+        self._db.delete(user)
+        self._db.commit()
+
+    def _get_user_or_raise(self, user_id: int) -> User:
+        user = self.get_user(user_id)
+        if not user:
+            raise NoResultFound
+        
+        return user

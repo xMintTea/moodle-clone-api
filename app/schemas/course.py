0d3e1bef7a9ae001644cacdc0929f2e007f531be
xMintTea <minttea77105110116@gmail.com>
2026-03-04 15:25:51 +0800

    feat: updated schemas

--- app/schemas/course.py
+++ app/schemas/course.py
@@ -1,12 +1,113 @@
-from pydantic import BaseModel, Field, EmailStr
+from pydantic import BaseModel, Field, EmailStr, ConfigDict
 from typing import Annotated, Optional
+from datetime import datetime
 
-class CourseSchema(BaseModel):
+
+
+# -------- Visibility --------
+
+class VisibilityBase(BaseModel):
+    name: Annotated[str, Field(...)]
+
+
+# -------- Course --------
+
+class CourseBase(BaseModel):
     name: Annotated[str, Field(...,min_length=4, max_length=256)]
     description: Annotated[str, Field()]
     secret: Annotated[Optional[str], Field()]
     visibility_id: Annotated[int, Field(..., ge=1)]
 
 
-class CourseSection(BaseModel):
+
+class CourseCreate(CourseBase):
+    ...
+    
+
+class CourseUpdate(CourseBase):
+    ...
+
+
+
+class CourseResponce(CourseBase):
+    ...
+    
+    
+    model_config = ConfigDict(from_attributes=True)
+
+
+
+# -------- Sections --------
+
+class SectionBase(BaseModel):
+    title: Annotated[str, Field(..., min_length=1, max_length=256)]
+    description: Annotated[Optional[str], Field()] = None
+    order: Annotated[int, Field(..., ge=0)]
+    visibility_id: Annotated[int, Field(..., ge=1)]
+
+
+class SectionCreate(SectionBase):
     course_id: Annotated[int, Field(..., ge=1)]
+
+
+class SectionUpdate(SectionCreate):
+    ...
+
+
+class SectionResponce(SectionBase):
+    id: Annotated[int, Field()]
+    course_id: Annotated[int, Field()]
+    creation_date: Annotated[datetime, Field()]
+    
+    model_config = ConfigDict(from_attributes=True)
+
+
+# -------- Sections --------
+
+class PageBase(BaseModel):
+    order: Annotated[int, Field(..., ge=0)]
+    visibility_id: Annotated[int, Field(..., ge=0)]
+
+
+
+class PageCreate(PageBase):
+    section_id: Annotated[int, Field(..., ge=1)]
+
+
+class PageUpdate(PageCreate):
+    ...
+
+
+class PageResponce(PageBase):
+    # TODO: gonna figure out how annotate this properly.
+    section_id: int
+    creation_date: datetime
+    last_change_date: datetime
+    
+    model_config = ConfigDict(from_attributes=True)
+
+
+
+# -------- Test --------
+
+class TestBase(BaseModel):
+    deadline_date: Annotated[Optional[datetime], Field()] = None
+    order: Annotated[int, Field(..., ge=0)]
+    visibility_id: Annotated[int, Field(..., ge=1)]
+
+
+class TestCreate(TestBase):
+    section_id: Annotated[int, Field(..., ge=1)]
+
+
+class TestUpdate(TestCreate):
+    ...
+    
+
+class TestResponce(TestBase):
+    id: int
+    section_id: int
+    creation_date: datetime
+    last_change_date: Optional[datetime]
+
+    model_config = ConfigDict(from_attributes=True)

3e2f231ff7cc3f9f3f08f97c7870d3b9c38ece6d
xMintTea <minttea77105110116@gmail.com>
2026-03-05 20:57:39 +0800

    feat: included tests router

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -4,6 +4,7 @@ from .users import router as users_router
 from .courses import router as courses_router
 from .sections import router as sections_router
 from .pages import router as pages_router
+from .tests import router as tests_router
 
 router = APIRouter(prefix="/v1")
 
@@ -11,3 +12,4 @@ router.include_router(users_router)
 router.include_router(courses_router)
 router.include_router(sections_router)
 router.include_router(pages_router)
+router.include_router(tests_router)

190499267bcc1015925ebea2dd58ce04ee5aedd2
xMintTea <minttea77105110116@gmail.com>
2026-03-05 20:57:13 +0800

    feat: included pages router

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -3,9 +3,11 @@ from fastapi import APIRouter
 from .users import router as users_router
 from .courses import router as courses_router
 from .sections import router as sections_router
+from .pages import router as pages_router
 
 router = APIRouter(prefix="/v1")
 
 router.include_router(users_router)
 router.include_router(courses_router)
 router.include_router(sections_router)
+router.include_router(pages_router)

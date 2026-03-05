4a9a2b770399c5119041130331995df8326e222c
xMintTea <minttea77105110116@gmail.com>
2026-03-05 20:56:26 +0800

    feat: included courses router

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from .users import router as users_router
+from .courses import router as courses_router
 
 router = APIRouter(prefix="/v1")
 
 router.include_router(users_router)
+router.include_router(courses_router)

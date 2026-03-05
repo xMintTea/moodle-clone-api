d41c4a6c0d1ae49a247cbb0afd2ffcb040d6faf3
xMintTea <minttea77105110116@gmail.com>
2026-03-05 21:00:50 +0800

    feat: rewrited user schemas

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field, EmailStr
+from pydantic import BaseModel, Field, EmailStr, ConfigDict
 from typing import Annotated
 
 from ..models.context.enums import UserStatus, UserType
@@ -6,7 +6,7 @@ from ..models.context.enums import UserStatus, UserType
 
 
 
-class UserSchema(BaseModel):
+class UserBase(BaseModel):
     first_name: Annotated[str,Field(..., title="Имя пользователя",
                                     min_length=2,
                                     max_length=50)]
@@ -16,7 +16,20 @@ class UserSchema(BaseModel):
     last_name: Annotated[str,Field(..., title="Фамилия пользователя",
                                     min_length=2,
                                     max_length=50)]
-    email: Annotated[EmailStr, Field(..., max_length=32)]
-    password: Annotated[str, Field(..., min_length=8, max_length=18)]
+
+
     user_type: Annotated[UserType, Field(...)]
     user_status: Annotated[UserStatus, Field(...)]
+
+
+class UserCreate(UserBase):
+    password: Annotated[str, Field(..., min_length=8, max_length=18)]
+    email: Annotated[EmailStr, Field(..., max_length=32)]
+
+
+class UserUpdate(UserCreate):
+    ...
+
+
+class UserResponse(UserBase):
+    model_config = ConfigDict(from_attributes=True)

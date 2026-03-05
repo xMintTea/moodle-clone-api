dc806f2dc88a0221da681ae521710d64d58070e0
xMintTea <minttea77105110116@gmail.com>
2026-03-05 20:51:16 +0800

    feat: initialized fastAPI app

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
-from database import Base, engine
+from .database import Base, engine
+from fastapi import FastAPI
 
+from .api.v1 import router as routerV1
+from .core.register_exception_handlers import register_exception_handlers
 
 Base.metadata.create_all(engine)
+
+app = FastAPI()
 

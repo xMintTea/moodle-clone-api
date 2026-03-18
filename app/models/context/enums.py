39031abc0b061fe6c30fea22c75b95f8de1ef845
xMintTea <minttea77105110116@gmail.com>
2026-03-18 18:14:05 +0800

    feat: updated enums

--- app/models/context/enums.py
+++ app/models/context/enums.py
@@ -1,11 +1,28 @@
-from enum import StrEnum
+from enum import IntEnum
 
-class UserStatus(StrEnum):
-    ACTIVE = "Активен"
-    SUSPENDED = "Остановлен"
+class UserStatus(IntEnum):
+    ACTIVE = 1
+    SUSPENDED = 2
 
 
-class UserType(StrEnum):
-    STUDENT = "Студент"
-    TEACHER = "Преподаватель"
-    ADMIN = "Администратор"
+class UserType(IntEnum):
+    DEFAULT = 1
+    REDACTOR = 2
+    ADMIN = 3
+    
+
+class CourseUserType(IntEnum):
+    STUDENT = 1
+    ASSISTENT = 2
+    TEACHER = 3
+
+
+class CourseAccessStatus(IntEnum):
+    GRANTED = 1
+    CLOSED = 2
+
+
+class Visibility(IntEnum):
+    VISIBLE_EVERYONE = 1
+    VISIBLE_TO_CREATOR = 2
+    NOT_VISIBLE = 3

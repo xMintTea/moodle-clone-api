7643229b9f0c77ff6dcdef0e68b6f6faa02a2c2a
xMintTea <minttea77105110116@gmail.com>
2026-03-18 18:22:44 +0800

    feat: added middleware

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from .database import Base, engine
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .api.v1 import router as routerV1
 from .core.register_exception_handlers import register_exception_handlers
@@ -10,5 +11,13 @@ app = FastAPI()
 register_exception_handlers(app)
 
 app.include_router(routerV1)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 

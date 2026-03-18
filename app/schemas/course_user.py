28657c20220fc850f2238d3d7e304981c08a2ca4
xMintTea <minttea77105110116@gmail.com>
2026-03-18 23:16:16 +0800

    feat: updated model fields

--- app/schemas/course_user.py
+++ app/schemas/course_user.py
@@ -4,17 +4,17 @@ from datetime import datetime
 
 from ..utils.schemas_utils import optional
 from .user import UserResponse
-from ..models.context.enums import CourseAccessStatus, CourseUserType
+from ..models.context.enums import CourseAccessStatus, CourseAccessLevel
 
 # -------- Course's user schemas --------
 
 class CourseUserBase(BaseModel):
-    access_lvl: Annotated[CourseUserType, Field(default=CourseUserType.STUDENT)]
-    access_status_id: Annotated[CourseAccessStatus, Field(default=CourseAccessStatus.GRANTED)]
+    access_level: Annotated[CourseAccessLevel, Field(default=CourseAccessLevel.STUDENT)]
+    access_status: Annotated[CourseAccessStatus, Field(default=CourseAccessStatus.GRANTED)]
 
 
 class CreateCourseUser(CourseUserBase):
-    ...
+    user_id: Annotated[int, Field(..., ge=1)]
 
 
 @optional
@@ -23,7 +23,7 @@ class UpdateCourseUser(CourseUserBase):
     
 
 
-class CourseUserResponse(CreateCourseUser):
+class CourseUserResponse(CourseUserBase):
     user_id: Annotated[int, Field(..., ge=1)]
     course_id: Annotated[int, Field(..., ge=1)]
     date_of_join: Annotated[datetime, Field(...)]

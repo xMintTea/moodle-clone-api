090b5f10d67de7fa5955c6910eca0a27c6581aee
xMintTea <minttea77105110116@gmail.com>
2026-03-25 19:33:26 +0800

    feat: added file service

---
+++ app/service/file_service.py
@@ -0,0 +1,70 @@
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import NoResultFound
+from datetime import datetime
+from typing import Optional
+from fastapi import UploadFile
+from fastapi.responses import StreamingResponse
+
+from ..models.file import File
+
+
+class FileService:
+    def __init__(self, session: Session) -> None:
+        self._db = session
+    
+    
+    def create_file(self, uploaded_file: UploadFile) -> File:
+        
+        bytes = uploaded_file.file.read()
+
+        file = File(
+            file_name=uploaded_file.filename,
+            content_type=uploaded_file.content_type,
+            headers=str(dict(uploaded_file.headers)),
+            size=uploaded_file.size,
+            file_bytes=bytes
+        )
+        
+        self._db.add(file)
+        self._db.commit()
+        self._db.refresh(file)
+        
+        return file
+    
+    
+    def get_file(self, file_id: int) -> Optional[File]:
+        return self._db.get(File, file_id)
+    
+    
+    def stream_file(self, file_id: int) -> StreamingResponse:
+        file = self._get_file_or_raise(file_id)
+        
+    
+            
+        def iterfile():
+            chunk_size = 1024 * 1024
+            data = file.file_bytes
+            
+            for i in range(0, len(data), chunk_size):
+                yield data[i:i+chunk_size]
+        
+        print(file.headers)
+        
+        headers = {
+            "Content-Disposition": f"attachment; filename={file.file_name}"
+        }
+        
+        return StreamingResponse(
+            iterfile(),
+            media_type=file.content_type,
+            headers=headers
+        )
+        
+    def _get_file_or_raise(self, file_id: int) -> File:
+        file = self.get_file(file_id)
+        
+        if not file:
+            raise NoResultFound
+        
+        return file

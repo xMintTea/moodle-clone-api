c92b0823bb576e8c655e9dd215564d06ee88e8e3
xMintTea <minttea77105110116@gmail.com>
2026-04-30 17:31:22 +0800

    feat: updated db url

--- app/core/config.py
+++ app/core/config.py
@@ -1,16 +1,19 @@
 from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
 class Config(BaseSettings):
     app_name: str = "MoodleClone"
     debug: bool = False
-    db_name: str = "project.db"
     
     @property
-    def db_url(self):
-        return f"sqlite:///./{self.db_name}"
+    def db_url(self) -> str:
+        url = os.getenv("DATABASE_URL")
+        if url is None:
+            raise ValueError("DATABASE_URL environment variable not set")
+        return url
 
 
 
